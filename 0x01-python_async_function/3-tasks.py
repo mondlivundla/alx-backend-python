eb94e562c2267cc59180fcc8e650aeb9729f68a2
mondlivundla <mondlivundla7@gmail.com>
2023-07-10 21:04:24 +0200

    3-tasks.py

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Async basics
+    Author - Mondliwethu Vundla
+"""
+
+from asyncio import Task, create_task
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Task:
+    """ Tasks """
+    task = create_task(wait_random(max_delay))
+    return task
