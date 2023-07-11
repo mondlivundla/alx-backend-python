95ae7065d01cb137fd1af6ebace8a7daf01e1afa
mondlivundla <mondlivundla7@gmail.com>
2023-07-11 20:39:30 +0200

    0-asyc_generator.py

---
+++ 0x02-python_async_comprehension/0-asyc_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+    Async Comprehensions
+    Author - Mondliwethu Vundla
+"""
+
+from asyncio import sleep
+from random import uniform
+from typing import Generator
+
+async def async_generator() -> Generator[float, None, None]:
+    """ ASYNC GENERATOR """
+    for _ in range(10):
+        await sleep(1)
+        yield uniform(0, 10)
